5492b36a4ed9a02ad62e48aefcaaf732c1c619de
sapna546 <nishadsapna546@gmail.com>
2026-08-22 15:18:00 +0530

    Time: 10 ms (13.75%), Space: 24.7 MB (14.73%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+        for (auto interval : intervals) {
+            
+            // No overlap
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            }
+            
+            // Overlap
+            else {
+                ans.back()[1] = max(ans.back()[1], interval[1]);
+            }
+        }
+
+        return ans;
+    }
+};
